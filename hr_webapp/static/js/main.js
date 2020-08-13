1812c69e20ef4148a28c42bcaaca2a70edc6826f
Paola Faith Simon <simon.paolafaith@gmail.com>
2020-08-13 08:52:22 +0800

    grouped rows by month-year

--- hr_webapp/static/js/main.js
+++ hr_webapp/static/js/main.js
@@ -1,17 +1,50 @@
 $(document).ready(function() {
     $('#checkin-table').DataTable( {
-        order: [[0, 'desc']],
-        responsive: true,
-        searching: false,
-        select: true,
-        // rowGroup: {
-        //     dataSrc: 0
-        // },
-        "aaSorting": [],
-        columnDefs: [{
-            orderable: false,
-            targets: [2, 3, 4]
+        order: [[0, 'desc']], //column to order, descending
+        responsive: true, //mobile friendly
+        searching: false, //disable search bar
+        select: true, //enable row selection
+        columnDefs: [{ 
+            orderable: false, //disable sorting of selected columns
+            targets: [2, 3, 4] //index of columns
         }],
+        rowGroup: {
+            // grouping rows based in their month-year
+            dataSrc: function ( row ) {
+                var date = row[0].split("-");
+                var month = date[1];
+                var year = date[0];
+
+                //this value is passed as 'group' in the startRender
+                return "" + month + ' ' + year; 
+            },
+            startRender: function ( rows, group ) {
+                month = group.split(" ")[0]*1;
+                year = group.split(" ")[1];
+
+                var months = [ "January", "February", "March", "April", "May", "June", 
+                    "July", "August", "September", "October", "November", "December" ];
+
+                var monthName = months[month-1];
+
+                // compute total hours for the group(month)
+                var totalHours = rows
+                    .data()
+                    .pluck(4)
+                    .reduce( function (a, b) {
+                        return a + b*1;
+                    }, 0);
+
+                //to address the floating point weirdness
+                totalHours = Math.round((totalHours + Number.EPSILON) * 100) / 100;
+ 
+                return $('<tr/>')
+                    .append( '<td colspan="3">' + monthName + ' ' + year + '</td>' )
+                    .append( '<td style="text-align:right;">Total Hours:</td>')
+                    .append( '<td>'+totalHours+'</td>' )
+            },
+            endRender: null
+        },
 
         //make it scrollable rather than paged
         scrollY: 400,
@@ -28,21 +61,16 @@ $(document).ready(function() {
         footerCallback: function (row, data, start, end, display) {
             var api = this.api(), data;
 
-            //string to int
-            var intVal = function(i) {
-                return typeof i === 'string' ?
-                    i*1 :
-                    typeof i === 'number' ?
-                        i:0;
-            };
-
             //total all hours
             totalHours = api
                 .column(4)
                 .data()
                 .reduce( function (a,b) {
-                    return intVal(a) + intVal(b);
+                    return a + b*1;
                 }, 0);
+
+            //to address the floating point weirdness
+            totalHours = Math.round((totalHours + Number.EPSILON) * 100) / 100;
             
             //update footer
             $( api.column(4).footer() ).html(
